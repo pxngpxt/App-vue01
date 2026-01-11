c51d2fb748644587561df55b667cad9e4d8cb33b
pxngpxt <pongpot2527@gmail.com>
2026-01-11 18:29:53 +0700

    update v2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
+  <div class="container">
     <h1>This is an about page</h1>
+    <h1>Pongpat Kunharat</h1>
   </div>
 </template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,37 @@
 <template >
-    <div>
+    <div class="container">
+<div class="text-primary">
         <h1>Contact to Pongpat Kunharat</h1>
         <h3>Tel.064-401-2942</h3>
         <img alt="Vue logo" src="../assets/g.jpg">
+<br>
+        <div class="container">
+  <div class="row">
+    <div class="col-sm-6 ">
+        <img width="100%" alt="Vue logo" src="../assets/V.jpg">
     </div>
+
+    <div class="col-sm-6 bg-warning ">
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Detail</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+
+        <div class="mb-3 text-center">
+            <button type="button" class="btn btn-primary btn-lg ">Submit</button>&nbsp;
+            <button type="button" class="btn btn-danger btn-lg">Cancel</button>
+        </div>
+
+    </div>
+  </div>
+</div>
+     <br><br><br>
+</div>
+</div>
 </template>
+
