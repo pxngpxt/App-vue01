b21beb686f17bbd74b302cd1101cf9fe1e7f59a1
pxngpxt <pongpot2527@gmail.com>
2026-02-15 16:27:54 +0700

    update v7_addDB

--- php_api/DB/db_shop.sql
+++ php_api/DB/db_shop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 10, 2026 at 02:17 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Feb 15, 2026 at 10:26 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -59,21 +59,22 @@ CREATE TABLE `employees` (
   `department` varchar(50) NOT NULL,
   `salary` decimal(10,2) NOT NULL,
   `active` tinyint(1) DEFAULT 1,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `image` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`emp_id`, `full_name`, `department`, `salary`, `active`, `created_at`) VALUES
-(0000000001, 'สมชาย ใจดี', 'IT', 35000.00, 1, '2026-01-18 10:28:08'),
-(0000000002, 'สมหญิง ดีงาม', 'บัญชี', 28000.00, 1, '2026-01-18 10:28:08'),
-(0000000003, 'อนันต์ สุขใจ', 'การตลาด', 25000.00, 0, '2026-01-18 10:28:08'),
-(0000000004, 'สุดา พรมดี', 'ทรัพยากรบุคคล', 32000.00, 1, '2026-01-18 10:28:08'),
-(0000000006, 'SXN', 'IT', 150000.00, 1, '2026-02-01 08:48:59'),
-(0000000007, 'Pongpat kunharat', 'CEO', 200000.00, 1, '2026-02-01 09:02:13'),
-(0000000008, 'sasd', 'asda', 56584.00, 1, '2026-02-01 09:32:26');
+INSERT INTO `employees` (`emp_id`, `full_name`, `department`, `salary`, `active`, `created_at`, `image`) VALUES
+(0000000001, 'สมชาย ใจดี', 'IT', '35000.00', 1, '2026-01-18 10:28:08', '1771147097_1771146656_male.png'),
+(0000000002, 'สมหญิง ดีงาม', 'บัญชี', '28000.00', 1, '2026-01-18 10:28:08', '1771147091_1771146646_female.png'),
+(0000000003, 'อนันต์ สุขใจ', 'การตลาด', '25000.00', 0, '2026-01-18 10:28:08', '1771146861_1771146656_male.png'),
+(0000000004, 'สุดา พรมดี', 'ทรัพยากรบุคคล', '32000.00', 1, '2026-01-18 10:28:08', '1771146851_1771146820_female.png'),
+(0000000006, 'SXN', 'IT', '150000.00', 1, '2026-02-01 08:48:59', '1771146820_female.png'),
+(0000000007, 'Pongpat kunharat', 'CEO', '200000.00', 1, '2026-02-01 09:02:13', '1771146656_male.png'),
+(0000000008, 'sasd', 'asda', '56584.00', 1, '2026-02-01 09:32:26', '1771146646_female.png');
 
 -- --------------------------------------------------------
 
@@ -96,9 +97,9 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `description`, `price`, `image`, `stock`, `created_at`) VALUES
-(00000000001, 'เสื้อยืดคอกลม', 'เสื้อยืดผ้าฝ้าย 100% สวมใส่สบาย', 199.00, 'P1.jpg', 50, '2026-01-25 10:46:59'),
-(00000000002, 'กางเกงยีนส์', 'กางเกงยีนส์ทรงกระบอก สีฟ้าอ่อน', 799.00, 'P2.jpg', 30, '2026-01-25 10:46:59'),
-(00000000003, 'รองเท้าผ้าใบ', 'รองเท้าผ้าใบสีขาว ใส่ได้ทุกโอกาส', 1299.00, 'P3.jpg', 20, '2026-01-25 10:46:59');
+(00000000001, 'เสื้อยืดคอกลม', 'เสื้อยืดผ้าฝ้าย 100% สวมใส่สบาย', '199.00', '1771145727_P1.jpg', 50, '2026-01-25 10:46:59'),
+(00000000002, 'กางเกงยีนส์', 'กางเกงยีนส์ทรงกระบอก สีฟ้าอ่อน', '799.00', '1771145717_P2.jpg', 30, '2026-01-25 10:46:59'),
+(00000000003, 'รองเท้าผ้าใบ', 'รองเท้าผ้าใบสีขาว ใส่ได้ทุกโอกาส', '1299.00', '1771145736_P3.jpg', 200, '2026-01-25 10:46:59');
 
 -- --------------------------------------------------------
 
@@ -123,7 +124,8 @@ INSERT INTO `student` (`student_id`, `first_name`, `last_name`, `phone`, `email`
 (2, 'Jane', 'Smith', '0819876543', 'jane.smith@example.com'),
 (3, 'Michael', 'Brown', '0823456789', 'michael.brown@example.com'),
 (4, 'Emily', 'Johnson', '0834567890', 'emily.johnson@example.com'),
-(5, 'Chris', 'Williams', '0845678901', 'chris.williams@example.com');
+(5, 'Chris', 'Williams', '0845678901', 'chris.williams@example.com'),
+(6, 'Pongpat', 'Kunharat', '0644012942', 'pongpat@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -192,7 +194,7 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `emp_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `emp_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -204,7 +206,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `type`
